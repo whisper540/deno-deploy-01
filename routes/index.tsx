39aa184e9e9f0c7a40150bb557faaa4e1abd27d8
10002236 <wushidong@inovance.com>
2022-07-14 19:41:17 +0800

    测试提交构建

--- routes/index.tsx
+++ routes/index.tsx
@@ -12,8 +12,9 @@ export default function Home() {
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
       <p class={tw`my-6`}>
-        璇宝，要给我留点牛肉呀！我马上就下班了！爱你呦❤️
+        这是一条deno-deploy自动构建的页面,文件基于github仓库！
       </p>
+        <p>基于Deno(1.23.4)+Fresh(1.0)</p>
       <Counter start={3} />
     </div>
   );
