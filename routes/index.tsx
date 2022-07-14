9bac201c6554b191862f18a88135a68f6ef91289
10002236 <wushidong@inovance.com>
2022-07-14 19:32:00 +0800

    测试更新

--- routes/index.tsx
+++ routes/index.tsx
@@ -12,8 +12,7 @@ export default function Home() {
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
       <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.dkal.
+        璇宝，要给我留点牛肉呀！我马上就下班了！爱你呦❤️
       </p>
       <Counter start={3} />
     </div>
