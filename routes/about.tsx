b51faf76aea4a4805ff4c76c0e9698bb93c70500
10002236 <wushidong@inovance.com>
2022-07-14 19:07:08 +0800

    first commit

---
+++ routes/about.tsx
@@ -0,0 +1,22 @@
+// routes/about
+
+/** @jsx h */
+import { h } from 'preact';
+import { Handlers } from '$fresh/server.ts';
+
+export const handler:Handlers = {
+    async GET(req, ctx) {
+        const resp = await ctx.render();
+        resp.headers.set("X-Custom-Header", "Hello");
+        return resp;
+    }
+};
+
+export default function AboutPage(){
+    return (
+        <main>
+            <h1>About Page</h1>
+            <p>This is about page.</p>
+        </main>
+    );
+}
